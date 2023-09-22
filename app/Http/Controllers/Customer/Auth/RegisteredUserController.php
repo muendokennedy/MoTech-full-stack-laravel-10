22c8d8a7c44501986a2be1a74b11f4978da488d4
muendokennedy <kennedymunyao999@gmail.com>
2023-09-22 10:33:21 +0300

    Modified the fields handled by the registered user controller

--- app/Http/Controllers/Customer/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Customer/Auth/RegisteredUserController.php
@@ -33,12 +33,16 @@ class RegisteredUserController extends Controller
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:'.User::class],
+            'phone' => ['required', 'string', 'max:255'],
+            'address' => ['required', 'string', 'max:255'],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
+            'phone' => $request->phone,
+            'address' => $request->address,
             'password' => Hash::make($request->password),
         ]);
 
