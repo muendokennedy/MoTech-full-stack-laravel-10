6b26ade12a4d29970756064316539cf429990c01
muendokennedy <kennedymunyao999@gmail.com>
2023-09-22 09:58:51 +0300

    Added the signup page and modified the forgot password blade page

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,5 +1,11 @@
-<x-guest-layout>
-    <div class="mb-4 text-sm text-gray-600 dark:text-gray-400">
+<x-app-layout>
+<section class="product-top pt-16 px-[4%] mx-auto lg:max-w-[1500px]">
+        <div
+        class="heading text-[#384857] border-b-2 text-base sm:text-xl font-semibold py-2 sm:py-4 capitalize"
+      >
+        Reset your <span class="text-[#68A4FE] px-2">password</span>
+      </div>
+    <div class="my-6 text-xs md:text-sm text-gray-600 dark:text-gray-400">
         {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
     </div>
 
@@ -10,16 +16,16 @@
         @csrf
 
         <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+        <div class="input-box my-4 w-full sm:basis-[48%]">
+              <label for="email" class="block pb-3 text-[#384857] text-sm md:text-base capitalize">Enter your email:</label>
+              <input type="text" name="email" id="email" class="text-sm sm:text-base px-4 py-2 w-full border-2 outline-none rounded-md focus:border-[#68A4FE] transition-all duration-300 ease-in-out" autofocus>
         </div>
 
         <div class="flex items-center justify-end mt-4">
-            <x-primary-button>
-                {{ __('Email Password Reset Link') }}
-            </x-primary-button>
+        <div class="input-box input-responsive my-4">
+            <button type="submit" class="text-center px-4 py-2 text-sm md:text-base rounded-md text-white bg-[#68a4fe] hover:bg-[#384857] transition-all duration-300 ease-in-out">{{ __('Email Password Reset Link') }}</button>
+        </div>
         </div>
     </form>
-</x-guest-layout>
+</section>
+</x-app-layout>
--- routes/auth.php
+++ routes/auth.php
@@ -1,26 +1,25 @@
 <?php
 
-use App\Http\Controllers\Auth\AuthenticatedSessionController;
-use App\Http\Controllers\Auth\ConfirmablePasswordController;
-use App\Http\Controllers\Auth\EmailVerificationNotificationController;
-use App\Http\Controllers\Auth\EmailVerificationPromptController;
-use App\Http\Controllers\Auth\NewPasswordController;
-use App\Http\Controllers\Auth\PasswordController;
-use App\Http\Controllers\Auth\PasswordResetLinkController;
-use App\Http\Controllers\Auth\RegisteredUserController;
-use App\Http\Controllers\Auth\VerifyEmailController;
+use App\Http\Controllers\Customer\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\Customer\Auth\ConfirmablePasswordController;
+use App\Http\Controllers\Customer\Auth\EmailVerificationNotificationController;
+use App\Http\Controllers\Customer\Auth\NewPasswordController;
+use App\Http\Controllers\Customer\Auth\PasswordController;
+use App\Http\Controllers\Customer\Auth\PasswordResetLinkController;
+use App\Http\Controllers\Customer\Auth\RegisteredUserController;
+use App\Http\Controllers\Customer\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
-    Route::get('register', [RegisteredUserController::class, 'create'])
-                ->name('register');
+    Route::get('signup/customer', [RegisteredUserController::class, 'create'])
+                ->name('customer.signup');
 
-    Route::post('register', [RegisteredUserController::class, 'store']);
+    Route::post('signup/customer', [RegisteredUserController::class, 'store']);
 
-    Route::get('login', [AuthenticatedSessionController::class, 'create'])
-                ->name('login');
+    Route::get('login/customer', [AuthenticatedSessionController::class, 'create'])
+                ->name('customer.login');
 
-    Route::post('login', [AuthenticatedSessionController::class, 'store']);
+    Route::post('login/customer', [AuthenticatedSessionController::class, 'store']);
 
     Route::get('forgot-password', [PasswordResetLinkController::class, 'create'])
                 ->name('password.request');
@@ -36,8 +35,6 @@ Route::middleware('guest')->group(function () {
 });
 
 Route::middleware('auth')->group(function () {
-    Route::get('verify-email', EmailVerificationPromptController::class)
-                ->name('verification.notice');
 
     Route::get('verify-email/{id}/{hash}', VerifyEmailController::class)
                 ->middleware(['signed', 'throttle:6,1'])
