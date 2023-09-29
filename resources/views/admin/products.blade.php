833ef7a3b007fb24a298414c53a94cd4e188c75a
muendokennedy <kennedymunyao999@gmail.com>
2023-09-29 15:18:41 +0300

    Changed the selected option using the laravel @selected directive as well as changed the styling of the input fields upon encountering an error

--- resources/views/admin/products.blade.php
+++ resources/views/admin/products.blade.php
@@ -156,12 +156,12 @@
         <div class="new-product bg-white p-4 rounded-md my-4">
           <h2 class="text-[rgb(4,46,255)] font-semibold text-base md:text-xl py-4 capitalize">add new product</h2>
           <div class="new-product-form">
-            <form action="{{route('product.store')}}" method="POST">
+            <form action="{{route('product.store')}}" method="POST" enctype="multipart/form-data">
             @csrf
               <div class="form-row w-full flex flex-col md:flex-row justify-between">
                 <div class="input-box md:basis-[48%]">
                   <label for="category" class="block py-3">Select Category:</label>
-                  <select name="category" id="category" class="@error('address') border-red-600 @enderror border-2 outline-none rounded-md px-4 py-2 w-full focus:border-[#042EFF] transition-all duration-300 ease-in-out">
+                  <select name="category" id="category" class="@error('category') border-red-500 @enderror border-2 outline-none rounded-md px-4 py-2 w-full focus:border-[#042EFF] transition-all duration-300 ease-in-out">
                     @if (!old('category'))
                     <option value="None" selected disabled hidden>Select a category</option>
                     @endif
@@ -175,7 +175,7 @@
                 </div>
                 <div class="input-box md:basis-[48%]">
                   <label for="productName" class="block py-3">Enter Product name:</label>
-                  <input type="text" name="productName" id="productName" value="{{old('productName')}}" class="px-2 py-2 rounded-md outline-none border-2 w-full focus:border-[#042EFF] transition-all duration-300 ease-in-out">
+                  <input type="text" name="productName" id="productName" value="{{old('productName')}}" class="@error('productName') border-red-500 @enderror px-2 py-2 rounded-md outline-none border-2 w-full focus:border-[#042EFF] transition-all duration-300 ease-in-out">
                   @error('productName')
                     <p class="text-red-500 text-sm sm:text-base py-2 w-full">{{$message}}</p>
                   @enderror
@@ -184,14 +184,14 @@
               <div class="form-row w-full flex flex-col md:flex-row justify-between">
                 <div class="input-box md:basis-[48%]">
                     <label for="initialPrice" class="block py-3">Enter the initial price:</label>
-                    <input type="number" name="initialPrice" id="initialPrice" value="{{old('initialPrice')}}" class="px-2 py-2 rounded-md outline-none border-2 w-full focus:border-[#042EFF] transition-all duration-300 ease-in-out">
+                    <input type="number" name="initialPrice" id="initialPrice" value="{{old('initialPrice')}}" class="@error('initialPrice') border-red-600 @enderror px-2 py-2 rounded-md outline-none border-2 w-full focus:border-[#042EFF] transition-all duration-300 ease-in-out">
                     @error('initialPrice')
                     <p class="text-red-500 text-sm sm:text-base py-2 w-full">{{$message}}</p>
                     @enderror
                 </div>
                 <div class="input-box md:basis-[48%]">
                   <label for="discountPrice" class="block py-3">Enter the discounted price:</label>
-                  <input type="number" name="discountPrice" id="discountPrice" value="{{old('discountPrice')}}" class="px-2 py-2 rounded-md outline-none border-2 w-full focus:border-[#042EFF] transition-all duration-300 ease-in-out">
+                  <input type="number" name="discountPrice" id="discountPrice" value="{{old('discountPrice')}}" class="@error('discountPrice') border-red-600 @enderror px-2 py-2 rounded-md outline-none border-2 w-full focus:border-[#042EFF] transition-all duration-300 ease-in-out">
                   @error('discountPrice')
                   <p class="text-red-500 text-sm sm:text-base py-2 w-full">{{$message}}</p>
                   @enderror
@@ -200,7 +200,7 @@
               <p class="capitalize mt-4">Select product images (Start with the primary image):</p>
               <div class="form-row w-full flex flex-col md:flex-row gap-2 justify-between my-4">
                 <div class="md:basis-[23%]">
-                    <div class="input-box1 file-box flex items-center justify-center flex-col  border-dashed border-2 border-[#042EFF]">
+                    <div class="@error('firstImage') border-red-600 @enderror input-box1 file-box flex items-center justify-center flex-col  border-dashed border-2 border-[#042EFF]">
                       <div class="original-info1 flex items-center justify-center flex-col">
                         <div class="icon pt-4">
                           <label for="firstImage" class="fa-solid fa-cloud-upload-alt text-4xl text-[#042EFF]"></label>
@@ -216,7 +216,7 @@
                 </div>
                   <input type="file" name="firstImage" id="firstImage" class="file1" hidden>
                   <div class="md:basis-[23%]">
-                      <div class="input-box2 file-box flex items-center justify-center flex-col  border-dashed border-2 border-[#042EFF]">
+                      <div class="@error('secondImage') border-red-600 @enderror input-box2 file-box flex items-center justify-center flex-col  border-dashed border-2 border-[#042EFF]">
                         <div class="original-info2 flex items-center justify-center flex-col">
                           <div class="icon pt-4">
                             <label for="secondImage" class="fa-solid fa-cloud-upload-alt text-4xl text-[#042EFF]"></label>
@@ -232,7 +232,7 @@
                   </div>
                   <input type="file" name="secondImage" id="secondImage" class="file2" hidden>
                   <div class="md:basis-[23%]">
-                      <div class="input-box3 md:basis-[23%]  file-box flex items-center justify-center flex-col  border-dashed border-2 border-[#042EFF]">
+                      <div class="@error('thirdImage') border-red-600 @enderror input-box3 md:basis-[23%]  file-box flex items-center justify-center flex-col  border-dashed border-2 border-[#042EFF]">
                         <div class="original-info3 flex items-center justify-center flex-col">
                           <div class="icon pt-4">
                             <label for="thirdImage" class="fa-solid fa-cloud-upload-alt text-4xl text-[#042EFF]"></label>
@@ -248,7 +248,7 @@
                   </div>
                   <input type="file" name="thirdImage" id="thirdImage" class="file3" hidden>
                   <div class="md:basis-[23%]">
-                      <div class="input-box4 md:basis-[23%]  file-box flex items-center justify-center flex-col  border-dashed border-2 border-[#042EFF]">
+                      <div class="@error('fourthImage') border-red-600 @enderror input-box4 md:basis-[23%]  file-box flex items-center justify-center flex-col  border-dashed border-2 border-[#042EFF]">
                         <div class="original-info4 flex items-center justify-center flex-col">
                           <div class="icon pt-4">
                             <label for="fourthImage" class="fa-solid fa-cloud-upload-alt text-4xl text-[#042EFF]"></label>
@@ -267,14 +267,14 @@
               <div class="form-row w-full flex flex-col md:flex-row justify-between">
                 <div class="input-box md:basis-[48%]">
                   <label for="specifications" class="block py-3">Enter product specifications (CSV):</label>
-                  <input type="text" name="specifications" id="specifications" value="{{old('specifications')}}" class="px-2 py-2 rounded-md outline-none border-2 w-full focus:border-[#042EFF] transition-all duration-300 ease-in-out">
+                  <input type="text" name="specifications" id="specifications" value="{{old('specifications')}}" class="@error('specifications') border-red-600 @enderror px-2 py-2 rounded-md outline-none border-2 w-full focus:border-[#042EFF] transition-all duration-300 ease-in-out">
                   @error('specifications')
                   <p class="text-red-500 text-sm sm:text-base py-2 w-full">{{$message}}</p>
                   @enderror
                 </div>
                 <div class="input-box md:basis-[48%]">
                   <label for="brandName" class="block py-3">Enter the brand name:</label>
-                  <input type="text" name="brandName" id="brandName" value="{{old('brandName')}}" class="px-2 py-2 rounded-md outline-none border-2 w-full focus:border-[#042EFF] transition-all duration-300 ease-in-out">
+                  <input type="text" name="brandName" id="brandName" value="{{old('brandName')}}" class="@error('brandName') border-red-600 @enderror px-2 py-2 rounded-md outline-none border-2 w-full focus:border-[#042EFF] transition-all duration-300 ease-in-out">
                   @error('brandName')
                   <p class="text-red-500 text-sm sm:text-base py-2 w-full">{{$message}}</p>
                   @enderror
@@ -283,7 +283,7 @@
               <div class="form-row">
                 <div class="input-box">
                   <label for="product-description" class="block py-3">Type the product description:</label>
-                  <textarea name="productDescription" id="product-description" cols="30" rows="10" class="px-2 py-2 rounded-md outline-none border-2 w-full focus:border-[#042EFF] transition-all duration-300 ease-in-out">{{old('productDescription')}}</textarea>
+                  <textarea name="productDescription" id="product-description" cols="30" rows="10" class="@error('productDescription') border-red-600 @enderror px-2 py-2 rounded-md outline-none border-2 w-full focus:border-[#042EFF] transition-all duration-300 ease-in-out">{{old('productDescription')}}</textarea>
                   @error('productDescription')
                   <p class="text-red-500 text-sm sm:text-base py-2 w-full">{{$message}}</p>
                   @enderror
