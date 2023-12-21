98afc5d509d9de5a053e23eaf19a406feaaf5b18
muendokennedy <kennedymunyao999@gmail.com>
2023-12-21 12:07:29 +0300

    Created and written the logic of the wishlist controller

--- app/Http/Controllers/WishlistController.php
+++ app/Http/Controllers/WishlistController.php
@@ -2,9 +2,37 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Wishlist;
 use Illuminate\Http\Request;
 
 class WishlistController extends Controller
 {
-    //
+    //Add products to the wishlist
+    public function addToWishlist(Request $request)
+    {
+        $jsonData = $request->getContent();
+
+        $product = json_decode($jsonData, true);
+
+        if(auth('web')->user()){
+
+            $productinWishlist = Wishlist::where('product_id', $product['id'])->get();
+
+            if($productinWishlist->count() === 0){
+
+                $wishlist = Wishlist::create([
+                    'user_id' => auth()->user()->id,
+                    'product_id'=> $product['id']
+                ]);
+
+                return response()->json(['status' => 'product added to wishlist successfully']);
+            } else {
+
+                return response()->json(['status' => 'The product is already in the wishlist']);
+            }
+        } else{
+
+            return response()->json(['status' => 'Login to continue']);
+        }
+    }
 }
