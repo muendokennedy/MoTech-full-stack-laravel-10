fb9427ffc6e0012f1e2b7427ed3c24751f91c6a3
muendokennedy <kennedymunyao999@gmail.com>
2023-12-21 12:11:12 +0300

    Created the foreign key columns in the wishlists table

--- database/migrations/2023_11_04_201558_create_wishlists_table.php
+++ database/migrations/2023_11_04_201558_create_wishlists_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('wishlists', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('product_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
